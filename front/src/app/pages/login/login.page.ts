3d2dadee026cf7f35fd592b1f3f84d7cd029f87f
Nicolas Marquez <nicomars270@gmail.com>
2024-10-22 01:10:33 -0300

    Quedo lo del login, no funciona porque hay fallos con los servicios

---
+++ front/src/app/pages/login/login.page.ts
@@ -0,0 +1,26 @@
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './login.page.html',
+  styleUrl: './login.page.css',
+})
+export class LoginPage {
+  username: string = '';
+  password: string = '';
+
+  authService: AuthService = inject(AuthService);
+  router: Router = inject(Router);
+
+  async onSubmitLogin() {
+    await this.authService.login(this.username, this.password);
+    if (this.authService.isValidUser()) {
+      this.router.navigate(['/temas']);
+    }
+  }
+}
